34d6277d5177149109d8f3e74360ce8466d53d33
Kyle Conway <Kojacked815@gmail.com>
2021-08-04 19:17:10 -0500

    Added java file opener

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,12 @@
 package com.company;
 
+import java.io.File;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Start start = new Start();
+        File file = start.run();
+        
     }
 }
---
+++ src/com/company/Start.java
@@ -0,0 +1,30 @@
+package com.company;
+
+
+import javax.swing.*;
+import java.io.File;
+import java.nio.file.Paths;
+
+public class Start {
+    File selectFile;
+
+    public File run(){
+        String path = Paths.get("PronounceKana.iml").toAbsolutePath().toString();
+        File f = new File(path);
+
+        JFileChooser jf = new JFileChooser(path);
+        jf.setCurrentDirectory(f);
+        int result = jf.showOpenDialog(jf.getParent());
+
+        if (result == JFileChooser.APPROVE_OPTION){
+            selectFile = jf.getSelectedFile();
+
+        }
+        return selectFile;
+
+
+    }
+
+
+
+}
